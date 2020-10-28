1de4f8e6315ccd83c75221f97e1408dd9e4d1e0e
LukeTheDank <lukecardozaisawesome@gmail.com>
2020-10-27 20:44:27 -0400

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,15 @@
+ var domparser=new DOMParser();
+      function loadimage(image){
+        $.ajax({
+          url: "https://cors-anywhere.herokuapp.com/https://www.google.com.ua/search?source=lnms&sa=X&gbv=1&tbm=isch&q="+image,
+          success: function(result) {
+
+            domparser=domparser.parseFromString(result,"text/html");
+            // Gets DOM element with image results
+                  let image = domparser.getElementsByTagName("img")[1].src;
+                  //document.write(image);
+                  document.getElementById("image").src=image;
+
+          }
+        });
+      }
